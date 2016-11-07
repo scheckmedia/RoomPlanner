551d7bbdeff5c881878657251a927796146e572c
Michel Neumann <neumann.michel93@gmail.com>
2016-11-07 23:41:00 +0100

    add camera access

--- RoomPlanner/CameraViewController.swift
+++ RoomPlanner/CameraViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
+
+/**
+    IMPORTANT: Will cause bad access errors if tested in the iOS Simulator!
+ */
 
 class CameraViewController: UIViewController {
-
+    
+    private let session = AVCaptureSession()
+    private var device: AVCaptureDevice?
+    private var preview: AVCaptureVideoPreviewLayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Set session quality to highest resolution
+        session.sessionPreset = AVCaptureSessionPresetPhoto
+        
+        // Fetch all available devices and return the back-facing camera only
+        for device in (AVCaptureDevice.devices() as! [AVCaptureDevice]) {
+            if device.hasMediaType(AVMediaTypeVideo) && device.position == AVCaptureDevicePosition.back {
+                self.device = device
+            }
+        }
+        
+        // WTF Error Handling?!
+        do {
+            session.addInput(try AVCaptureDeviceInput(device: self.device))
+        } catch _ {
+            print("Could not connect to AVCaptureDevice!")
+        }
+        
+        // Attach camera preview to this view controller (for testing)
+        self.preview = AVCaptureVideoPreviewLayer(session: self.session)
+        self.view.layer.addSublayer(self.preview!)
+        self.preview?.frame = self.view.layer.frame
+        
+        self.session.startRunning()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
