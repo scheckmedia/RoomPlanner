1edba29c1a26b3e14e7d0d4f9b558d8163c38e8a
Felix Brix <felix.brix@onlinehome.de>
2016-11-23 09:45:01 +0100

    Fixed cells

--- RoomPlanner/ModelMenuController.swift
+++ RoomPlanner/ModelMenuController.swift
@@ -9,18 +9,37 @@
 import Foundation
 import UIKit
 
-class ModelMenuController: UICollectionViewController {
-    func collectionView(collectionView: UICollectionView, cellForItemAt indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath)
-        let imageview:UIImageView = UIImageView(frame: CGRect(x: 50, y: 50, width: self.view.frame.width - 200, height: 50))
-        
-        let model = ModelObject.all()?[indexPath.row]
-        print(ModelObject.all())
-        print(model ?? "")
-        let image:UIImage = UIImage(named: "")!
-        imageview.image = image
-        cell.contentView.addSubview(imageview)
-        
-        return cell
+class ModelMenuController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+
+//    func collectionView(collectionView: UICollectionView, cellForItemAt indexPath: NSIndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath)
+//        let imageview:UIImageView = UIImageView(frame: CGRect(x: 50, y: 50, width: self.view.frame.width - 200, height: 50))
+//        
+//        let model = ModelObject.all()?[indexPath.row]
+//        print(ModelObject.all())
+//        print(model ?? "")
+//        let image:UIImage = UIImage(named: "")!
+//        imageview.image = image
+//        cell.contentView.addSubview(imageview)
+//        
+//        return cell
+//    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath as IndexPath) 
+    cell.backgroundColor = UIColor.red
+    
+    return cell
     }
+    
+    private func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        print("123lol")
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        print("456lol")
+        return 10
+    }
+    
 }
